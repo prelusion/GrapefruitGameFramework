35ac29af49712e253a471bd09ab8cee425cfd7cf
Leon <leonjelsma@gmail.com>
2020-03-29 12:29:32 +0200

    wip

--- src/main/java/com/grapefruit/gamework/app/controller/ControllerGame.java
+++ src/main/java/com/grapefruit/gamework/app/controller/ControllerGame.java
@@ -1,35 +1,29 @@
 package com.grapefruit.gamework.app.controller;
 
 import com.grapefruit.gamework.app.model.IModel;
+import com.grapefruit.gamework.app.model.ModelGame;
 import com.grapefruit.gamework.app.model.ModelGameTile;
-
+import com.grapefruit.gamework.framework.Board;
+import com.grapefruit.gamework.framework.Tile;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-
-import javafx.scene.effect.BlurType;
-import javafx.scene.effect.InnerShadow;
+import javafx.geometry.Insets;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Text;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class ControllerGameTile implements IController{
+public class ControllerGame implements IController{
 
 
-    private ModelGameTile model;
-
-
-    @FXML
-    private HBox gameTile;
+    private ModelGame model;
 
     @FXML
-    private ImageView gameIcon;
-
-    @FXML
-    private Text gameName;
+    private VBox gameBoard;
 
     @FXML
     private URL location;
@@ -40,7 +34,7 @@ public class ControllerGameTile implements IController{
     /**
      * Required for FXML
      */
-    public ControllerGameTile()
+    public ControllerGame()
     {
     }
 
@@ -50,63 +44,69 @@ public class ControllerGameTile implements IController{
      */
     private void initialize()
     {
-        gameName.setText(model.getGame().getName());
-        gameIcon.setImage(model.getGame().getIcon());
     }
 
-    /**
-     * Called when mouse enters tile to updates hover effect.
-     *
-     * @Param event supplied by FXML
-     */
-    @FXML
-    private void mouseEnter(MouseEvent event){
-        if (!model.isSelected()) {
-            gameTile.getStyleClass().set(0, "menu-item-hover");
-        } else {
-            gameTile.getStyleClass().set(0, "menu-item-hover-selected");
-        }
-    }
 
-    /**
-     * Called when mouse enters tile to updates hover effect.
-     *
-     * @Param event supplied by FXML
-     */
-    @FXML
-    private void mouseExit(){
-        if (!model.isSelected()) {
-            gameTile.getStyleClass().set(0, "menu-item-normal");
-        } else {
-            gameTile.getStyleClass().set(0, "menu-item-selected");
-
-        }
-    }
-    /**
-     * Called when mouse enters tile to updates selected effect.
-     *
-     * @Param event supplied by FXML
-     */
-    @FXML
-    private void mouseClicked(MouseEvent event){
-        if (model.isSelected()){
-            model.gameUnselected();
-        } else {
-            model.gameSelected();
-        }
-    }
 
     /**
      * {@inheritDoc}
      */
     @Override
     public void setModel(IModel model) {
-        this.model = (ModelGameTile) model;
-        if (this.model.isSelected()) {
-            gameTile.getStyleClass().set(0, "menu-item-selected");
-        } else {
-            gameTile.getStyleClass().set(0, "menu-item-normal");
+        this.model = (ModelGame) model;
+        drawBoard(this.model.getGame().getBoard(), false);
+    }
+
+    private void drawBoard(Board board, boolean checkered){
+        GridPane gridPane = new GridPane();
+        Tile[][] tiles = board.getGrid();
+        int tileSize = 80;
+
+        boolean toggle = false;
+
+        for (Tile[] column: tiles){
+            for (Tile tile: column){
+                Pane pane;
+                if (checkered) {
+                    if (toggle) {
+                        pane = createBoardTile(tileSize, Color.rgb(232, 214, 202));
+                        toggle = !toggle;
+                    } else {
+                        pane = createBoardTile(tileSize, Color.rgb(59, 41, 29));
+                        toggle = !toggle;
+                    }
+                } else {
+                    pane = createBoardTile(tileSize, Color.GREEN);
+                }
+                gridPane.add(pane, tile.getX(), tile.getY(), 1, 1);
+            }
+            toggle = !toggle;
         }
-        initialize();
+
+        gameBoard.getChildren().removeAll(gameBoard.getChildren());
+        gameBoard.getChildren().add(gridPane);
+    }
+
+    private Pane createBoardTile(int size, Color color){
+        Pane pane = new Pane();
+        pane.setMinSize(size,size);
+        pane.setPrefSize(size,size);
+        pane.setBackground(new Background(new BackgroundFill(color, CornerRadii.EMPTY, Insets.EMPTY)));
+        pane.getStyleClass().add(0, "game-board-tile");
+
+        pane.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                pane.getStyleClass().set(0, "game-board-tile-hover");
+            }
+        });
+
+        pane.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                pane.getStyleClass().set(0, "game-board-tile");
+            }
+        });
+        return pane;
     }
 }

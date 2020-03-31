58a70aea688d5303efb6c829fc5279dcde5132f7
Delano van der Waal <d.g.d.van.der.waal@st.hanze.nl>
2020-03-31 11:46:12 +0200

    Renamed Class

--- src/main/java/com/grapefruit/gamework/framework/AI.java
+++
@@ -1,8 +0,0 @@
-package com.grapefruit.gamework.framework;
-
-public class AI extends Player {
-    public AI(String name, String color) {
-        super(name, color);
-    }
-
-}
---
+++ src/main/java/com/grapefruit/gamework/framework/Computer.java
@@ -0,0 +1,8 @@
+package com.grapefruit.gamework.framework;
+
+public class Computer extends Player {
+    public Computer(String name, String color) {
+        super(name, color);
+    }
+
+}

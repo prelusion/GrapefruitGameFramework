b3231b3c0d3f860c19e5fbc6caa7bee4ed9f65f4
Jarno Jellesma <jarnojellesma@gmail.com>
2020-04-01 14:13:13 +0200

    Add pseudocode to generate strategic values

--- src/main/java/com/grapefruit/gamework/games/reversi/ReversiFactory.java
+++ src/main/java/com/grapefruit/gamework/games/reversi/ReversiFactory.java
@@ -7,6 +7,8 @@ import com.grapefruit.gamework.framework.player.Player;
 
 public class ReversiFactory extends GameFactory {
     public static Reversi create(Player playerWhite, Player playerBlack) {
+//        int[] strategicValues = new int[];
+
         Board board = new ReversiBoard(9);
         int turnTimeout = 10;
         return new Reversi(board, playerWhite, playerBlack, turnTimeout);
@@ -19,3 +21,46 @@ public class ReversiFactory extends GameFactory {
         return new Reversi(board, players[0], players[1], turnTimeout);
     }
 }
+
+/*
+Calculate strategic values Reversi
+
+(row, col)
+
+99
+min, min
+min, max
+max, min
+max, max
+
+-8
+
+min, max-1
+min, min+1
+max, min+1
+max, min-1
+min+1, min
+max-1, max
+max-1, min
+max-1, max
+
+8
+min, max-2
+min, min+2
+max, min+2
+max, min-2
+min+2, min
+max-2, max
+max-2, min
+max-2, max
+
+6
+min, min+3
+min, max-3
+max, min+3
+max, min-3
+min+3, min
+max-3, max
+max-3, min
+max-3, max
+ */

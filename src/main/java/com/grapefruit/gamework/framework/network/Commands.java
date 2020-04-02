3b80adfe8cfae148c96115eba243e4ab53c7c972
Leon <leonjelsma@gmail.com>
2020-04-02 12:06:46 +0200

    added commands

---
+++ src/main/java/com/grapefruit/gamework/framework/network/Commands.java
@@ -0,0 +1,24 @@
+package com.grapefruit.gamework.framework.network;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Commands {
+
+    private static String KEYWORD_LOGIN = "login ";
+    private static String KEYWORD_LOGOUT = "logout";
+    private static String KEYWORD_GAMELIST = "get gamelist";
+
+    public static void login(ServerConnection conn, String name){
+        conn.sendCommand(KEYWORD_LOGIN + name);
+    }
+
+    public static void logout(ServerConnection conn){
+        conn.sendCommand(KEYWORD_LOGOUT);
+    }
+
+    public static List<String> getGameList(ServerConnection conn){
+        conn.sendCommand(KEYWORD_GAMELIST);
+        return new ArrayList<>();
+    }
+}

724e5ffbcfa0fcd5e40ad69cc1b331877c708514
Jarno Jellesma <jarnojellesma@gmail.com>
2020-04-03 14:06:46 +0200

    Fix reversi factory board size bug

--- src/main/java/com/grapefruit/gamework/games/reversi/ReversiFactory.java
+++ src/main/java/com/grapefruit/gamework/games/reversi/ReversiFactory.java
@@ -6,15 +6,17 @@ import com.grapefruit.gamework.framework.GameFactory;
 import com.grapefruit.gamework.framework.player.Player;
 
 public class ReversiFactory extends GameFactory {
+    static final int DEFAULT_REVERSI_BOARD_SIZE = 8;
+
     public Reversi create(Player playerWhite, Player playerBlack) {
-        Board board = new ReversiBoard(9);
+        Board board = new ReversiBoard(DEFAULT_REVERSI_BOARD_SIZE);
         int turnTimeout = 10;
         return new Reversi(board, playerWhite, playerBlack, turnTimeout);
     }
 
     @Override
     public Game create(Player[] players) {
-        Board board = new ReversiBoard(9);
+        Board board = new ReversiBoard(DEFAULT_REVERSI_BOARD_SIZE);
         int turnTimeout = 10;
         return new Reversi(board, players[0], players[1], turnTimeout);
     }

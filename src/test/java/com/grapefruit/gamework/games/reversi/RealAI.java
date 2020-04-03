80e7f83ef679adc78cf43cca3173d3de71c8d4bc
Jarno Jellesma <jarnojellesma@gmail.com>
2020-04-03 09:30:19 +0200

    clean RealAI test code

--- src/test/java/com/grapefruit/gamework/games/reversi/RealAI.java
+++ src/test/java/com/grapefruit/gamework/games/reversi/RealAI.java
@@ -12,65 +12,54 @@ import com.grapefruit.gamework.framework.player.Player;
 
 public class RealAI {
     /* AI vs AI */
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         int turnTimeout = 10;
-        Scanner in = new Scanner(System.in);
         Board board = new ReversiBoard(8);
-        AIPlayer playerWhite = new AIPlayer("White", Colors.WHITE, new MinimaxAlgorithm(), 7);
+        AIPlayer playerWhite = new AIPlayer("White", Colors.WHITE, new MinimaxAlgorithm(), 3);
         AIPlayer playerBlack = new AIPlayer("Black", Colors.BLACK, new MinimaxAlgorithm(), 1);
         Reversi game = new Reversi(board, playerWhite, playerBlack, turnTimeout);
 
-        System.out.println("INIT BOARD:");
-        board.printBoard();
-        System.out.println("STRATEGIC VALUES");
+        System.out.println("strategic values:");
         board.printStrategicValues();
 
+        int turnCount = 1;
         while (!game.hasFinished()) {
-//            Thread.sleep(50);
-            System.out.println("\n\n----------------NEW TURN--------------------");
+            System.out.println("current board:");
+            board.printBoard();
+
+            System.out.println(String.format("\n\n----------------turn %s--------------------", turnCount));
 
             Player currentPlayer = game.getCurrentPlayer();
             System.out.println("current player: " + currentPlayer.getColor());
+
             List<Tile> availableMoves = game.getAvailableMoves(currentPlayer);
             System.out.println("available moves: " + availableMoves.size());
             board.printAvailableMoves(currentPlayer);
 
-            Player opponent;
-            if (currentPlayer == playerWhite) {
-                opponent = playerBlack;
-            } else {
-                opponent = playerWhite;
-            }
-            Tile move = ((AIPlayer) currentPlayer).calculateMove(game.getBoard(), opponent);
-
-            if (move == null) {
-                System.out.println("WTF MOVE IS NULL");
-                game.nextPlayer();
-                game.checkFinished();
+            if (availableMoves.size() == 0) {
+                System.out.println("no moves available");
                 continue;
             }
 
+            Player opponent = currentPlayer == playerWhite ? playerBlack : playerWhite;
+
+            Tile move = ((AIPlayer) currentPlayer).calculateMove(game.getBoard(), opponent);
+
+            System.out.println(String.format("play move: %s %s", move.getRow(), move.getCol()));
             game.playMove(move.getRow(), move.getCol(), currentPlayer);
-            System.out.println("Row " + move.getRow() + " Col " + move.getCol() + " Player " + currentPlayer.getColor());
-            System.out.println(game.getBoard().getGrid()[move.getRow()][move.getCol()].getPlayer());
-
-            System.out.println();
-
-            if (availableMoves.size() > 0) {
-                System.out.println("current board");
-                board.printBoard();
-            }
-//            System.exit(0);
         }
 
+        System.out.println("-----------------------------------");
+        System.out.println("game finished");
+
         Game.GameResult result = game.getGameResult();
-        System.out.println("Result: " + result);
+        System.out.println("result: " + result);
+
         if (game.hasWinner()) {
             System.out.println("Winner: " + game.getWinner().getColor().toString());
         }
 
-        System.out.println("STRATEGIC VALUES");
+        System.out.println("strategic values:");
         board.printStrategicValues();
     }
 }
-

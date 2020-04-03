317625eae7aef07d8ead405e917f3d0dad65ba02
Jarno Jellesma <jarnojellesma@gmail.com>
2020-04-03 14:22:15 +0200

    Improve Reversi factory

--- src/main/java/com/grapefruit/gamework/games/reversi/ReversiFactory.java
+++ src/main/java/com/grapefruit/gamework/games/reversi/ReversiFactory.java
@@ -8,16 +8,14 @@ import com.grapefruit.gamework.framework.player.Player;
 public class ReversiFactory extends GameFactory {
     static final int DEFAULT_REVERSI_BOARD_SIZE = 8;
 
+    @Override
+    public Game create(Player[] players) {
+        return this.create(players[0], players[1]);
+    }
+
     public Reversi create(Player playerWhite, Player playerBlack) {
         Board board = new ReversiBoard(DEFAULT_REVERSI_BOARD_SIZE);
         int turnTimeout = 10;
         return new Reversi(board, playerWhite, playerBlack, turnTimeout);
-    }
-
-    @Override
-    public Game create(Player[] players) {
-        Board board = new ReversiBoard(DEFAULT_REVERSI_BOARD_SIZE);
-        int turnTimeout = 10;
-        return new Reversi(board, players[0], players[1], turnTimeout);
     }
 }

6ad73cb6fbe379ec68d3cea68076f84ec6f7583b
Jarno Jellesma <jarnojellesma@gmail.com>
2020-04-09 11:32:08 +0200

    Ingame score statistics works

--- src/main/java/com/grapefruit/gamework/app/controller/ControllerGame.java
+++ src/main/java/com/grapefruit/gamework/app/controller/ControllerGame.java
@@ -71,6 +71,18 @@ public class ControllerGame implements IController {
     private Text currentColor;
 
     @FXML
+    private Text playerNameA;
+
+    @FXML
+    private Text playerNameB;
+
+    @FXML
+    private Text playerScoreA;
+
+    @FXML
+    private Text playerScoreB;
+
+    @FXML
     private VBox gameBoard;
 
     @FXML
@@ -271,21 +283,16 @@ public class ControllerGame implements IController {
     }
 
     private void updateInfo() {
-        scorePlayerName.getChildren().removeAll(scorePlayerName.getChildren());
-        scorePlayerScore.getChildren().removeAll(scorePlayerScore.getChildren());
-
-        for (Player ignored : game.getPlayers()) {
-            scorePlayerName.getChildren().add(new Text("PlayerName"));
-            scorePlayerScore.getChildren().add(new Text("100"));
-        }
-
         currentColor.setText(game.getCurrentPlayer().getColor().toString());
         currentPlayerName.setText(game.getCurrentPlayer().getName());
         turnNumber.setText(Integer.toString(game.getTurnCount()));
-
         timeLeft.setText(String.valueOf(game.getTurnSecondsLeft()));
-        //Todo implement turn number
-//        turnNumber.setText("99");
+
+        Player[] players = game.getPlayers();
+        playerNameA.setText(players[0].getName());
+        playerScoreA.setText(Integer.toString(game.getBoard().countPieces(players[0])));
+        playerNameB.setText(players[1].getName());
+        playerScoreB.setText(Integer.toString(game.getBoard().countPieces(players[1])));
     }
 
     public void checkFinished() {

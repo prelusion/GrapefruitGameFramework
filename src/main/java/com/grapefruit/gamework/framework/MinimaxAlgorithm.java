a23aa27e4cbebe863f22101ea6d899d371f5185a
Delano van der Waal <d.g.d.van.der.waal@st.hanze.nl>
2020-04-10 11:27:31 +0200

    Fixed AI depth

--- src/main/java/com/grapefruit/gamework/framework/MinimaxAlgorithm.java
+++ src/main/java/com/grapefruit/gamework/framework/MinimaxAlgorithm.java
@@ -60,6 +60,69 @@ public class MinimaxAlgorithm {
         }
     }
 
+    private Tile realCalculateBestMove(Board board, Player player, Player opponent, int turnCount, boolean firstTurn, int depth) {
+        this.player = player;
+        this.opponent = opponent;
+
+        if (turnCount > 44) {
+            System.out.println("increase depth (turn > 44)");
+            currentDepth++;
+        }
+
+        if (firstTurn) {
+            turnCountDecrease(turnCount);
+        }
+
+
+        Map<Tile, Integer> tiles = threadedMiniMax(board, player, depth);
+
+        Tile bestTile = null;
+        int bestScore = MIN_VALUE;
+        for (Map.Entry<Tile, Integer> entry : tiles.entrySet()) {
+            if (entry.getValue() > bestScore) {
+                bestTile = entry.getKey();
+                bestScore = entry.getValue();
+
+            } else if (bestTile != null &&
+                            entry.getKey().getStrategicValue() > bestTile.getStrategicValue() &&
+                            entry.getValue() == bestScore)
+            {
+                bestTile = entry.getKey();
+                bestScore = entry.getValue();
+            }
+        }
+
+        if (firstTurn && secondsLeft() >= 8) {
+            currentDepth++;
+            System.out.println("increase depth");
+        }
+
+        if(!isTimedOut() && secondsLeft() >= 1 && depth < 30) {
+            timeoutStack.push(isTimedOut());
+
+            System.out.println("depth: " + depth);
+            Tile newTile = realCalculateBestMove(board, player, opponent, turnCount, false, depth + 1);
+            System.out.println(timeoutStack);
+            if(!timeoutStack.isEmpty()) {
+                if (newTile != null && !timeoutStack.pop()) {
+                    System.out.println("new best tile on depth " + depth);
+                    bestTile = newTile;
+                    timeoutStack.clear();
+                } else {
+                    System.out.println("corrupt tile, ignoring");
+                }
+            }
+        }
+
+        if (firstTurn && secondsLeft() <= 1) {
+            System.out.println("decrease depth");
+            currentDepth--;
+        }
+
+        return bestTile;
+    }
+
+
     private Map<Tile, Integer> threadedMiniMax(Board board, Player player, int depth) {
         ArrayList<Thread> threads = new ArrayList<>();
         List<Tile> moves = board.getAvailableMoves(player);
@@ -93,83 +156,18 @@ public class MinimaxAlgorithm {
         return tiles;
     }
 
-    private Tile realCalculateBestMove(Board board, Player player, Player opponent, int turnCount, boolean firstTurn, int depth) {
-        this.player = player;
-        this.opponent = opponent;
-
-        if (turnCount > 44) {
-            System.out.println("increase depth (turn > 44)");
-            currentDepth++;
-        }
-
-        turnCountDecrease(turnCount);
-
-        System.out.println("depth: " + depth);
-
-        Map<Tile, Integer> tiles = threadedMiniMax(board, player, depth);
-
-        Tile bestTile = null;
-        int bestScore = MIN_VALUE;
-        for (Map.Entry<Tile, Integer> entry : tiles.entrySet()) {
-            if (entry.getValue() > bestScore) {
-                bestTile = entry.getKey();
-                bestScore = entry.getValue();
-
-            } else if (bestTile != null &&
-                            entry.getKey().getStrategicValue() > bestTile.getStrategicValue() &&
-                            entry.getValue() == bestScore)
-            {
-                bestTile = entry.getKey();
-                bestScore = entry.getValue();
-            }
-        }
-
-        System.out.println("timeout: " + isTimedOut());
-        System.out.println("seconds left: " + secondsLeft());
-
-        if (firstTurn && secondsLeft() >= 8) {
-            currentDepth++;
-            System.out.println("increase depth");
-        }
-
-        if(!isTimedOut() && secondsLeft() >= 1) {
-            timeoutStack.push(isTimedOut());
-
-            System.out.println("recursion");
-            Tile newTile = realCalculateBestMove(board, player, opponent, turnCount, false, depth + 1);
-            if(!timeoutStack.isEmpty()) {
-                if (newTile != null && !timeoutStack.pop()) {
-                    System.out.println("new best tile on depth " + depth);
-                    bestTile = newTile;
-                    timeoutStack.clear();
-                } else {
-                    System.out.println("corrupt tile, ignoring");
-                }
-            }
-        }
-
-        if (firstTurn && secondsLeft() <= 1) {
-            System.out.println("decrease depth");
-            currentDepth--;
-        }
-
-        return bestTile;
-    }
-
     public void startTimeout(int timeout) {
         this.timeout = timeout;
         startTime = getCurrentSeconds();
 
         timeoutThread = new Thread(() -> {
             try {
-                System.out.println("sleeping");
                 Thread.sleep(timeout);
-                System.out.println("after sleep");
                 timedOut = true;
                 timeoutStack.push(isTimedOut());
-                System.out.println("timed out");
+                System.out.println("TIMEOUTPUSHED");
+                System.out.println("TIMEOUTPUSHED");
             } catch (InterruptedException ignored) {
-                System.out.println("timeout thread interrupted, ignoring");
             }
         });
 
